bf1da1a741e58e8ead6047ebb0c0252aec0b7cae
Udeogu Alex <alexanderudeogu@gmail.com>
2024-08-14 11:41:11 +0100

    First main commit

---
+++ oil-e-commerce/src/App.tsx
@@ -0,0 +1,15 @@
+import MobNavbar from "./components/MobNavbar"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <MobNavbar />
+      <Hero />
+    </main>
+  )
+}
+
+export default App
---
+++ oil-e-commerce/src/components/Category.tsx
@@ -0,0 +1,21 @@
+const data =[
+    {
+        id: 0,
+        name: "Fresh Fruits",
+        count: "9 Products",
+        img: "/intro-img.png"
+    }
+]
+
+
+
+
+const Category = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Category
